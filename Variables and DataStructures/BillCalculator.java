321cff46af38fe69f7eed8f7c2d46259832cdf15
Jayashish01 <jayashish180@gmail.com>
2024-09-27 21:44:45 +0530

    intial

---
+++ Variables and DataStructures/BillCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class BillCalculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the of a pencil: ");
+        float pencilCost = sc.nextFloat();
+
+        System.out.println("Enter the cost of a pen: ");
+        float penCost = sc.nextFloat();
+
+        System.out.println("Enter the cot of an eraser: ");
+        float eraserCost = sc.nextFloat();
+
+        float sum =  pencilCost + penCost + eraserCost;
+        float GST = sum + (18*sum)/100;
+        System.out.println(sum);
+        System.out.println(GST);
+    }
+}
---
+++ Variables and DataStructures/Variables2.java
@@ -0,0 +1,5 @@
+public class Variables2 {
+    public static  void main(String[] args) {
+        
+    }
+}
