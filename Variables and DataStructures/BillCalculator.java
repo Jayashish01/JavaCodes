2c3fe8aa61810518518809235963d070563dc301
Jayashish01 <jayashish180@gmail.com>
2024-10-01 22:29:23 +0530

    commit

--- Variables and DataStructures/BillCalculator.java
+++ Variables and DataStructures/BillCalculator.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 
 public class BillCalculator{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter the of a pencil: ");
-        float pencilCost = sc.nextFloat();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the of a pencil: ");
+            float pencilCost = sc.nextFloat();
 
-        System.out.println("Enter the cost of a pen: ");
-        float penCost = sc.nextFloat();
+            System.out.println("Enter the cost of a pen: ");
+            float penCost = sc.nextFloat();
 
-        System.out.println("Enter the cot of an eraser: ");
-        float eraserCost = sc.nextFloat();
+            System.out.println("Enter the cot of an eraser: ");
+            float eraserCost = sc.nextFloat();
 
-        float sum =  pencilCost + penCost + eraserCost; //Jayashish
-        
-        float GST = sum + (18*sum)/100;
-        System.out.println(sum);
-        System.out.println(GST);
+            float sum =  pencilCost + penCost + eraserCost; //Jayashish
+            
+            float GST = sum + (18*sum)/100;
+            System.out.println(sum);
+            System.out.println(GST);
+        }
     }
 }
---
+++ Variables and DataStructures/TypeCating.java
@@ -0,0 +1,7 @@
+public class TypeCating {
+    public static void main(String[]args){
+        float a = 25.225f;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
