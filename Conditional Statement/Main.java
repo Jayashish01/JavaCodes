166433169e4413bdcffd94ddd0ae8735d4d7dde1
Jayashish <jayashish180@gmail.com>
2024-10-05 15:17:12 +0530

    commit

---
+++ Conditional Statement/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 20;
+        
+        if ( A >= B ){
+            System.out.println("Largest number is A");
+        }
+        else {
+            System.out.println("Largest number is B");
+        }
+    }
+}
