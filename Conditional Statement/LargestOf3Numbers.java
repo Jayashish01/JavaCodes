4cf90fa9b1dccd472e029b2154511f4fa273aaca
Jayashish01 <jayashish180@gmail.com>
2024-12-14 10:01:01 +0530

    commit

---
+++ Conditional Statement/LargestOf3Numbers.java
@@ -0,0 +1,15 @@
+public class LargestOf3Numbers {
+    public static void main(String[] args) {
+        int A = 1, B = 2, C = 3;
+        
+        if ( ( A >= B ) && ( A >= C) ) {
+            System.out.println("largest is A");
+        }
+        else if ( B>= C ) {
+            System.out.println("largest is B");
+        }
+        else {
+            System.out.println("largest is C");
+        }
+    }
+}
