722ffcc86449e4e96bff966f97cd59d8c434e2d9
Jayashish01 <jayashish180@gmail.com>
2025-01-04 09:46:03 +0530

    commit

---
+++ Variables and DataStructures/Sub.java
@@ -0,0 +1,8 @@
+public class Sub {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int Sub = a-b;
+        System.out.println(Sub);
+    }
+}
---
+++ Variables and DataStructures/multiple.java
@@ -0,0 +1,8 @@
+public class multiple {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int product = a * b;
+        System.out.println("The product of " + a + " and " + b + " is " +  product);
+    }
+}
