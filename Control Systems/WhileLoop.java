e77f81d6dfdb5acab58081ff9c8c93a9aabd89eb
Jayashish01 <jayashish180@gmail.com>
2025-01-11 10:22:13 +0530

    "Updated WhileLoop.java to change print statement formatting."

--- Control Systems/WhileLoop.java
+++ Control Systems/WhileLoop.java
@@ -1,7 +1,7 @@
 public class WhileLoop {
     public static void main(String[] args) {
         int i = 0;
-        System.out.println("Printing the list of first 10 even numbers \n");
+        System.out.println("Printing the list of first 10 even numbers ");
         while(i<=10) {
             System.out.println(i);
             i = i + 2;
